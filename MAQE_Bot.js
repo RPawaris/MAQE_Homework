647abe8df0c34207bc72bb70db25ed53cafc089d
RPawaris <ikqpawarich@gmail.com>
2024-07-02 17:36:01 +0700

    Left Right

--- MAQE_Bot.js
+++ MAQE_Bot.js
@@ -1,18 +1,46 @@
 const args = process.argv.slice(2);
 
 if (args.length > 0) {
-  const input = args[0];
+  const inputs = args[0];
   const regex = /^[0-9LlWwRr]+$/;
 
-  if (regex.test(input)){
-    console.log(input);
-    const splited_input = input.split('');
-    console.log(splited_input)
-  } else {
-    console.log('Invalid. Input only contain numbers and L,W,R');
+  if (!regex.test(inputs)) {
+    console.log("Invalid. Input only contain numbers and L,W,R");
+    process.exit(1);
   }
-  
 
+  let x = 0,y = 0;
+  let direction = "North";
+  const directions = ["North", "East", "South", "West"];
+  let i = 0;
+  while (i < inputs.length) {
+    let input = inputs[i];
+    if (input === "L" || input === "l" || input === "R" || input === "r") {
+      let index = directions.indexOf(direction);
+      if (input === "L" || input === "l") {
+        direction = directions[(index + 3) % 4];
+      }else if (input === "R" || input === "r"){
+        direction = directions[(index + 1) % 4];
+      }
+      i++;
+    } else if (input === "W" || input === "w") {
+      i++;
+      let str_steps = '';
+      while (i< input.length && /[0-9]/.test(input[i])){
+        str_steps+= input[i];
+        i++;
+      }
+      let steps = parseInt(str_steps, 10);
+      console.log(steps)
+    } else {
+      console.log(input);
+      i++;
+      // console.log('Invalid. Input only contain numbers and L,W,R');
+      // process.exit(1);
+    }
+  }
+  console.log(`X: ${x} Y: ${y} Direction: ${direction}`)
 } else {
-  console.log('X: 0 Y: 0 Direction: North');
+  console.log("X: 0 Y: 0 Direction: North");
+  process.exit(1);
 }
